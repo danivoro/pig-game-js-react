6e4fbb1db4f8e24d522c15e0b5f337bb7b157f85
dani voro <mr.voro@gmail.com>
2023-07-07 09:24:17 +0200

    code as we finished it in live session

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+/**  TODO: 
+ -add win state
+ -reset the game after game over */
 
 function App() {
+  const [lastRoll, setLastRoll] = useState("-");
+  const [turnTotal, setTurnTotal] = useState(0);
+  const [player1Score, setPlayer1Score] = useState(0);
+  const [player2Score, setPlayer2Score] = useState(0);
+  const [whoseTurnItIs, setTurn] = useState(1);
+  const winThreshold = 20;
+
+  function handleRollClicked(){
+    const roll = generateDieRoll();
+    setLastRoll(prevRoll => roll);
+    if(roll === 1){
+      setTurnTotal(0); 
+      changePlayerTurn()
+    }else{
+      setTurnTotal((prevRoll) => prevRoll + roll);
+    }
+  }
+
+  function handleStickClicked(){
+    if(whoseTurnItIs === 1){
+      setPlayer1Score((prevScore) => prevScore + turnTotal);
+    }else{
+      setPlayer2Score((prevScore) => prevScore + turnTotal);
+    }
+    changePlayerTurn()
+    setTurnTotal(0);
+  }
+
+  function changePlayerTurn(){
+    setTurn((currPlayer) => (currPlayer === 1 ? 2 : 1))
+  }
+
+  function generateDieRoll(){
+    return pick([1,2,3,4,5,6]);
+  }
+
+  function classesForPlayersScoreSection(sectionPlayer){
+    const classes = ['playerScore']
+    if(sectionPlayer === whoseTurnItIs) classes.push('active')
+    return classes.join(' ')
+  }
+
+  function isGameOver(){
+    return (player1Score >= winThreshold || player2Score >= winThreshold)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pig Game!</h1>
+      { isGameOver() === true && <div className='gameOver'>GAME OVER!</div> }
+      <div className='Scores'>
+        <div className={classesForPlayersScoreSection(1)}>
+          Player 1 Score: {player1Score} {whoseTurnItIs === 1 && <span>⭐</span>}
+          </div>
+        <div className={classesForPlayersScoreSection(2)}>  
+          Player 2 Score: {player2Score} {whoseTurnItIs === 2  && <span>⭐</span>}
+          </div>
+      </div>
+      <div className='buttomHalf'>
+        <button onClick={handleRollClicked}>Roll!</button>
+        <div className='lastRoll'>
+          Last Roll: {lastRoll ? lastRoll : '-'}
+        </div>
+        <div className='turnTotal'>Turn total: {turnTotal}</div>
+        <button onClick={handleStickClicked}>Stick!</button>
+      </div>
     </div>
   );
+}
+
+function pick(inputArray){
+  const ix = Math.floor(Math.random() * inputArray.length)
+  return inputArray[ix];
 }
 
 export default App;
